cd33f69badcfd292cb059c09cc8f10c37b1f033a
Lyuboslav Ignatov <lyuboslavw@gmail.com>
2024-01-06 11:17:35 +0200

    auth controller refactored

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,54 +1,45 @@
-const router = require('express').Router();
 const { validationResult } = require('express-validator');
 
 const authService = require('../services/authService');
-const { registerUserValidator } = require('../validators/userValidator');
-const { isAuthenticated, isGuest } = require('../middlewares/authMiddleware');
-const { setAuthTokenAsCookie, clearAuthCookie } = require('../utils/authHelpers');
-const { createError, configError } = require('../utils/errorHelpers');
+const { StateValidationError } = require('../errors');
 
-
-router.get('/login', isGuest, (req, res) => {
+const loginPage = (req, res) => {
     res.render('auth/login');
-});
+};
 
-router.post('/login', isGuest, async (req, res, next) => {
+const login = async (req, res) => {
     const { email, password } = req.body;
 
-    try {
-        const user = await authService.login(email, password);
-        const authToken = await authService.generateAuthToken(user);
-        setAuthTokenAsCookie(res, authToken);
-        res.redirect('/');
-    } catch (error) {
-        next(createError(error, 400, 'auth/login'));
-    }
-});
+    const user = await authService.login(email, password);
+    const authorizationToken = await authService.generateAuthorizationToken(user);
 
-router.get('/register', isGuest, (req, res) => {
-    res.render('auth/register');
-});
-
-router.post('/register', isGuest, registerUserValidator, async (req, res, next) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-        configError(errors, 400, 'auth/register', req.body);
-        return next(errors);
-    }
-
-    try {
-        await authService.create(req.body);
-        res.redirect('/');
-    } catch (error) {
-        configError(error, 400, 'auth/register', req.body);
-        return next(error);
-    }
-});
-
-router.get('/logout', isAuthenticated, (req, res) => {
-    clearAuthCookie(res);
+    res.authorizationTokenCookie(authorizationToken);
     res.redirect('/');
-});
+};
 
-module.exports = router;
+const registerPage = (req, res) => {
+    res.render('auth/register');
+};
+
+const register = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        throw new StateValidationError(errors.errors);
+    }
+
+    await authService.register(req.body);
+    res.redirect('/');
+};
+
+const logout = (req, res) => {
+    res.clearAuthorizationCookie();
+    res.redirect('/');
+};
+
+module.exports = {
+    loginPage,
+    login,
+    registerPage,
+    register,
+    logout
+};
