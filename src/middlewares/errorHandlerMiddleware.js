34a8de2315f483a966ff1834c87c91f830280f0a
Lyuboslav Ignatov <lyuboslavw@gmail.com>
2024-01-09 18:07:01 +0200

    error handler middleware extended with different error types

--- src/middlewares/errorHandlerMiddleware.js
+++ src/middlewares/errorHandlerMiddleware.js
@@ -1,12 +1,58 @@
-const { normalizeError } = require('../utils/errorHelpers');
+const { MongoServerError } = require('mongodb');
+const { ValidationError, CastError } = require('mongoose').Error;
+const {
+    NotFoundError,
+    InvalidStateError,
+    AlreadyExistError,
+    ForbiddenError,
+    UnauthorizedError,
+    StateValidationError
+} = require('../errors');
+
+const createError = (message, status) => {
+    return {
+        message, status
+    };
+};
+
+const errorTypeHandlers = {
+    [NotFoundError.name]: (err) => createError(err.message, 404),
+    [InvalidStateError.name]: (err) => createError(err.message, 400),
+    [AlreadyExistError.name]: (err) => createError(err.message, 409),
+    [ForbiddenError.name]: (err) => createError(err.message, 403),
+    [UnauthorizedError.name]: (err) => createError(err.message, 401),
+    [ValidationError.name]: (err) => {
+        const message = Object.values(err.errors)[0].message;
+        return createError(message, 400);
+    },
+    [CastError.name]: (err) => {
+        if (err.kind === 'ObjectId') {
+            return createError("Invalid ID format", 400);
+        }
+        return createError("Invalid field value", 400);
+    },
+    [MongoServerError.name]: (err) => createError('Document already exists', 409),
+    [StateValidationError.name]: (err) => {
+        const message = err.errors[0].msg;
+        return createError(message, 400);
+    },
+    default: (err) => createError("Server error!", 500, "404", {})
+};
+
+exports.showViewOnError = (view) => {
+    return (req, res, next) => {
+        res.locals.errorView = view;
+        next();
+    };
+};
 
 exports.errorHandler = (err, req, res, next) => {
-    err = normalizeError(err);
+    const errorHandler = errorTypeHandlers[err.name] || errorTypeHandlers.default;
 
-    const status = err.status || 404;
-    const page = err.page || '404';
-    const message = err.message || "Not found";
-    const data = err.data || {};
+    const { status, message } = errorHandler(err);
+    const page = res.locals.errorView || '404';
+    const data = req.body || {};
 
-    res.status(status).render(page, { error: message, ...data });
+    res.status(status)
+        .render(page, { error: message, ...data });
 };
